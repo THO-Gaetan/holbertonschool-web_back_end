889aa07e691ee7d7c95ed3dd3fb577c29c48dfdf
THO-Gaetan <9771@holbertonstudents.com>
2025-03-28 17:31:37 +0100

    first try exercise 7

---
+++ ES6_classes/7-airport.js
@@ -0,0 +1,17 @@
+export default class Airport {
+    constructor(name, code) {
+        this._name = name;
+        this._code = code;
+    }
+
+    get code() {
+        return this._code;
+    }
+
+    set code(value) {
+        this._code = value;
+    }
+    toString() {
+        return `[object ${this.code}]`;
+    }
+}
