ea42899f97160a8f460b391a2b41b51463d89514
THO-Gaetan <9771@holbertonstudents.com>
2025-03-28 17:32:35 +0100

    fixing the syntaxe

--- ES6_classes/7-airport.js
+++ ES6_classes/7-airport.js
@@ -1,25 +1,18 @@
 export default class Airport {
-    constructor(name, code) {
-        this._name = name;
-        this._code = code;
-    }
+  constructor(name, code) {
+    this._name = name;
+    this._code = code;
+  }
 
-    get name() {
-        return this._name;
-    }
+  get code() {
+    return this._code;
+  }
 
-    set name(value) {
-        this._name = value;
-    }
+  set code(value) {
+    this._code = value;
+  }
 
-    get code() {
-        return this._code;
-    }
-
-    set code(value) {
-        this._code = value;
-    }
-    toString() {
-        return `[object ${this.code}]`;
-    }
+  toString() {
+    return `[object ${this.code}]`;
+  }
 }
