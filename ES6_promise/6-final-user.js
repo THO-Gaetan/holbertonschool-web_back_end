4ed24611caee49261ef22f4676f6e905d0f750f7
THO-Gaetan <9771@holbertonstudents.com>
2025-04-02 16:44:53 +0200

    first try exercise 6

---
+++ ES6_promise/6-final-user.js
@@ -0,0 +1,15 @@
+import signeUpUser from './5-photo-reject';
+import uploadPhoto from './4-user-promise';
+
+export default function handleProfileSignup(firstName, lastName, fileName) {
+  return Promise.all([
+    uploadPhoto(fileName),
+    signeUpUser(firstName, lastName),
+  ])
+    .then((values) => {
+      const photo = values[0];
+      const user = values[1];
+      console.log(`${user.firstName} ${user.lastName} ${photo.body}`);
+    })
+    .catch(() => {});
+}
