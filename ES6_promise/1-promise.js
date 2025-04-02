4342529a19ae61537b888fe92094ef30068c2896
THO-Gaetan <9771@holbertonstudents.com>
2025-04-02 14:36:58 +0200

    first try exercise 1

---
+++ ES6_promise/1-promise.js
@@ -0,0 +1,14 @@
+export default function getResponseFromAPI() {
+    return new Promise((resolve, reject) => {
+    const success = true;
+    if (success) {
+        resolve({
+            status: 200,
+            body: 'Success',
+        });
+        } 
+    else {
+        reject(new Error('The fake API is not working currently'));
+        }
+    });
+}
