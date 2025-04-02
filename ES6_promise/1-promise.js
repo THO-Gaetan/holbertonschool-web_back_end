dee7d284b7ab0dd5d9e9bc6900a897fc07500aa8
THO-Gaetan <9771@holbertonstudents.com>
2025-04-02 14:38:37 +0200

    first try exercise 1

--- ES6_promise/1-promise.js
+++ ES6_promise/1-promise.js
@@ -1,14 +1,12 @@
-export default function getResponseFromAPI() {
-    return new Promise((resolve, reject) => {
-    const success = true;
+export default function getResponseFromAPI(success) {
+  return new Promise((resolve, reject) => {
     if (success) {
-        resolve({
-            status: 200,
-            body: 'Success',
-        });
-        } 
-    else {
-        reject(new Error('The fake API is not working currently'));
-        }
-    });
+      resolve({
+        status: 200,
+        body: 'Success',
+      });
+    } else {
+      reject(new Error('The fake API is not working currently'));
+    }
+  });
 }
