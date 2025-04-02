55eb93c59180bb8fc614fa21d574816479369f37
THO-Gaetan <9771@holbertonstudents.com>
2025-04-02 13:38:55 +0200

    fixing syntaxe

--- ES6_promise/0-promise.js
+++ ES6_promise/0-promise.js
@@ -1,5 +1,10 @@
-function getResponseFromAPI() {
-    return new Promise(() => {});
+export default function getResponseFromAPI() {
+  return new Promise((resolve, reject) => {
+    const sucess = true;
+    if (sucess) {
+      resolve('Success');
+    } else {
+      reject(new Error('failed'));
+    }
+  });
 }
-
-export default getResponseFromAPI;
