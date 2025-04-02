e9caf07bc55b3e0827479d09cae01571526360c2
THO-Gaetan <9771@holbertonstudents.com>
2025-04-02 13:32:03 +0200

    make the code shorter

--- ES6_promise/0-promise.js
+++ ES6_promise/0-promise.js
@@ -1,10 +1,5 @@
-export default function getResponseFromAPI() {
-    return new Promise((resolve, reject) => {
-        const sucess = true;
-        if (sucess) {
-            resolve('Success');
-        } else {
-            reject(new Error('Failed'));
-        }
-    });
+function getResponseFromAPI() {
+    return new Promise(() => {});
 }
+
+export default getResponseFromAPI;
