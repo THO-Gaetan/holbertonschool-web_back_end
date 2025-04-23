bdd25ff43e437aeed230eb3ae5e1838cc08d2288
THO-Gaetan <9771@holbertonstudents.com>
2025-04-23 16:55:15 +0200

    pycodestyle

--- python_variable_annotations/3-to_str.py
+++ python_variable_annotations/3-to_str.py
@@ -3,6 +3,8 @@
 """to_str module.
 This module contains a function that converts a float to a string.
 """
+
+
 def to_str(n: float) -> str:
     """Convert a float to a string."""
     return str(n)
