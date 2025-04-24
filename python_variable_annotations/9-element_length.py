554aa9a53991bd7fc9d17b2d7bfdbceb7c749f22
THO-Gaetan <9771@holbertonstudents.com>
2025-04-24 10:16:35 +0200

    first try exercise 9

--- python_variable_annotations/9-element_length.py
+++ python_variable_annotations/9-element_length.py
@@ -1,1 +1,12 @@
 #!/usr/bin/env python3
+
+"""This module contains a function that sums a mixed list of integers
+and floats.It uses type annotations to specify the expected input and
+output types.The function takes a list of integers and floats
+as input and returns their sum as a float."""
+from typing import Iterable, List, Sequence, Tuple
+
+
+def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
+    """Returns a list of the lengths of each element in the input list."""
+    return [len(i) for i in lst]
