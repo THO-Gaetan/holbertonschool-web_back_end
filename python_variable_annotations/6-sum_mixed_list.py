10142741ce59925d057da57228f59dd301f37c67
THO-Gaetan <9771@holbertonstudents.com>
2025-04-24 10:02:39 +0200

    fixing pycodestyle and annotated parameter

--- python_variable_annotations/6-sum_mixed_list.py
+++ python_variable_annotations/6-sum_mixed_list.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
 
-"""This module contains a function that sums a mixed list of integers and floats.
-It uses type annotations to specify the expected input and output types."""
+"""This module contains a function that sums a mixed list of
+integers and floats.It uses type annotations to specify
+the expected input and output types."""
 from typing import List, Union
+
 
 def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
     """Returns the sum of a mixed list of integers and floats."""
--- python_variable_annotations/7-to_kv.py
+++ python_variable_annotations/7-to_kv.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
 
-"""This module contains a function that sums a mixed list of integers and floats.
+"""This module contains a function that converts a key and a value to a tuple.
 It uses type annotations to specify the expected input and output types."""
+from typing import Union, Tuple
 
-def to_kv(k, v):
+
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
     """Returns a tuple with the key and the square of the value."""
     return (k, v ** 2)
