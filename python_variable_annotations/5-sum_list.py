195a5cfcf740f447df6e60bd11508b426cbbf312
THO-Gaetan <9771@holbertonstudents.com>
2025-04-24 09:55:26 +0200

    fixing the syntaxe of exercise 5 and 6, first try exercise 7

--- python_variable_annotations/5-sum_list.py
+++ python_variable_annotations/5-sum_list.py
@@ -1,5 +1,9 @@
 #!/usr/bin/env python3
 
-def sum_list(input_list: list[float]) -> float:
+"""This module contains a function that sums a mixed list of integers and floats.
+It uses type annotations to specify the expected input and output types."""
+from typing import List
+
+def sum_list(input_list: List[float]) -> float:
     """Returns the sum of a list of floats."""
     return sum(input_list)
--- python_variable_annotations/7-to_kv.py
+++ python_variable_annotations/7-to_kv.py
@@ -1,1 +1,8 @@
 #!/usr/bin/env python3
+
+"""This module contains a function that sums a mixed list of integers and floats.
+It uses type annotations to specify the expected input and output types."""
+
+def to_kv(k, v):
+    """Returns a tuple with the key and the square of the value."""
+    return (k, v ** 2)
