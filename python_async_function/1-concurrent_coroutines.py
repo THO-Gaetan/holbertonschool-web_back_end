628b8e681589349c99004a5c62d6aebe2a83817a
THO-Gaetan <9771@holbertonstudents.com>
2025-04-25 20:57:23 +0200

    first try exercise 0 to 4

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -1,1 +1,12 @@
 #!/usr/bin/env python3
+"""returns a random delay between 0 and max_delay"""
+import asyncio
+from typing import List
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+async def wait_n(n: int, max_delay: int) -> List[float]:
+    """returns a list of delays"""
+    tasks = [wait_random(max_delay) for _ in range(n)]
+    delays = await asyncio.gather(*tasks)
+    return sorted(delays)
--- python_async_function/2-measure_runtime.py
+++ python_async_function/2-measure_runtime.py
@@ -1,1 +1,13 @@
 #!/usr/bin/env python3
+"""Module to measure the runtime of wait_n function"""
+wait_n = __import__('1-concurrent_coroutines').wait_n
+import time
+import asyncio
+
+def measure_time(n: int, max_delay: int) -> float:
+    """returns the total time it takes to execute wait_n"""
+    start_time = time.time()
+    asyncio.run(wait_n(n, max_delay))
+    end_time = time.time()
+    total_time = end_time - start_time
+    return total_time / n
