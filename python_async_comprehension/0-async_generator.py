db4bfa332b14f574f2760b29d21208a7f9465cdd
THO-Gaetan <9771@holbertonstudents.com>
2025-04-30 04:34:32 +0200

    pycodestyle

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -10,6 +10,7 @@ import asyncio
 import random
 from typing import Generator
 
+
 async def async_generator() -> Generator[float, None, None]:  # type: ignore
     """Yields a random number between 0 and 10 every second."""
     for _ in range(10):
