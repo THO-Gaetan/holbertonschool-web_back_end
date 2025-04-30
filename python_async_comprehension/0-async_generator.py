0ff0d9ab2bd72d92c6d4c2e706b75ed726935f75
THO-Gaetan <9771@holbertonstudents.com>
2025-04-30 04:27:39 +0200

    first try exercise 1-2, exercise 0

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -8,9 +8,9 @@ random floating-point numbers with a delay between each yield.
 """
 import asyncio
 import random
+from typing import Generator
 
-
-async def async_generator():
+async def async_generator() -> Generator[float, None, None]:  # type: ignore
     """Yields a random number between 0 and 10 every second."""
     for _ in range(10):
         await asyncio.sleep(1)
--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -1,1 +1,17 @@
 #!/usr/bin/env python3
+
+"""
+Asynchronous Comprehension Module
+
+This module demonstrates the use of async comprehensions to collect
+values from an asynchronous generator.
+"""
+import asyncio
+from typing import List
+
+async_generator = __import__('0-async_generator').async_generator
+
+
+async def async_comprehension() -> List[float]:
+    """Async comprehension that returns 10 random numbers"""
+    return [i async for i in async_generator()]
