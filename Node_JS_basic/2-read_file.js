374ac6cf25abb6c5516e72269d82f43cc6d8c32d
THO-Gaetan <9771@holbertonstudents.com>
2025-05-22 04:14:55 +0200

    first try exercise 2

---
+++ Node_JS_basic/2-read_file.js
@@ -0,0 +1,35 @@
+const fs = require('fs');
+/**
+ * countStudents - Reads a CSV file and counts the number of students in each field.
+ * @param {string} path - The path to the CSV file.
+ * @returns {void}
+ */
+function countStudents(path) {
+    try {
+        const data = fs.readFileSync(path, 'utf8');
+        const lines = data.split('\n').slice(1);
+        const students = {};
+        let totalStudents = 0;
+
+        for (const line of lines) {
+            const [firstName, field] = line.trim().split(',');
+            if (field) {
+                if (!students[field]) {
+                    students[field] = [];
+                }
+                students[field].push(firstName);
+                totalStudents++;
+            }
+        }
+
+        console.log(`Number of students: ${totalStudents}`);
+        for (const field in students) {
+            console.log(`Number of students in ${field}: ${students[field].length}. List: ${students[field].join(', ')}`);
+        }
+    }
+    catch (error) {
+        console.error('Cannot load the database');
+    }
+}
+
+module.exports = countStudents;
