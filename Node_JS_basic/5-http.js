2a1eb8a303a630bd644cb4c9243f3bc62a203f34
THO-Gaetan <9771@holbertonstudents.com>
2025-05-23 22:48:26 +0200

    fixing the use of end when write was needed

--- Node_JS_basic/5-http.js
+++ Node_JS_basic/5-http.js
@@ -15,10 +15,10 @@ function countStudents(filePath) {
         const students = lines.slice(1).map((line) => line.split(','));
         const studentCount = students.length;
         const fieldCounts = {};
-        let result = '';
+        let result = [];  // Use an array to collect lines
 
-        // Add student count to result string
-        result += `Number of students: ${studentCount}\n`;
+        // Add student count as first line
+        result.push(`Number of students: ${studentCount}`);
 
         for (const student of students) {
           const field = student[3];
@@ -29,11 +29,11 @@ function countStudents(filePath) {
         }
 
         for (const [field, names] of Object.entries(fieldCounts)) {
-          result += `Number of students in ${field}: ${names.length}. List: ${names.join(', ')}\n`;
+          result.push(`Number of students in ${field}: ${names.length}. List: ${names.join(', ')}`);
         }
         
-        // Now resolve with the result string instead of just logging to console
-        resolve(result);
+        // Join with newlines but don't add an extra one at the end
+        resolve(result.join('\n'));
       }
     });
   });
@@ -49,7 +49,7 @@ const app = http.createServer((req, res) => {
         res.end('Hello Holberton School!');
     } else if (reqpath === '/students') {
         const databaseFilePath = process.argv[2] || 'database.csv';
-        res.end('This is the list of our students\n');
+        res.write('This is the list of our students\n');
         countStudents(databaseFilePath)
             .then((data) => {
             res.end(data)
